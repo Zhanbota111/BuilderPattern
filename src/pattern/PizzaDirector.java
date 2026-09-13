b898746b8a129ed1504e297580043548b4d34663
User <252443@astanait.edu.kz>
2026-09-13 07:00:41 +0500

    feat: complete Builder pattern implementation for Pizza application

--- src/pattern/PizzaDirector.java
+++ src/pattern/PizzaDirector.java
@@ -1,37 +1,18 @@
 package pattern;
 
 public class PizzaDirector {
-    public Pizza makeClassicMargherita(PizzaBuilder builder) {
-        return builder.setDough("Thin Crust")
-                .setSauce("Tomato Sauce")
-                .setCheese("Mozzarella")
-          фpackage pattern;
-
-        public class PizzaDirector {
-            public Pizza makeLargeSpicyPizza(PizzaBuilder builder) {
-                return builder.reset()
-                        .setSize(40)
-                        .setSauce("Spicy Tomato")
-                        .addTopping("Jalapeno")
-                        .setExtraCheese(true)
-                        .build();
-            }
-
-            public Pizza makeStandardPizza(PizzaBuilder builder) {
-                return builder.reset()
-                        .setSize(30)
-                        .build();
-            }
-        }      .addTopping("Fresh Basil")
+    public Pizza makeLargeSpicyPizza(PizzaBuilder builder) {
+        return builder.reset()
+                .setSize(40)
+                .setSauce("Spicy Tomato")
+                .addTopping("Jalapeno")
+                .setExtraCheese(true)
                 .build();
     }
 
-    public Pizza makeSpicyPepperoni(PizzaBuilder builder) {
-        return builder.setDough("Thick Crust")
-                .setSauce("Spicy Tomato Sauce")
-                .setCheese("Mozzarella")
-                .addTopping("Pepperoni Slices")
-                .addTopping("Jalapenos")
+    public Pizza makeStandardPizza(PizzaBuilder builder) {
+        return builder.reset()
+                .setSize(30)
                 .build();
     }
 }
